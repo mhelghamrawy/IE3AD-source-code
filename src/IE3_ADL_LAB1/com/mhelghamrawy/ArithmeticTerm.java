82428e923aa93ab39fcc7e53a1317bd86b614c52
Mohamed ElGhamrawy <mhelghamrawy@gmail.com>
2020-10-31 11:52:15 +0100

    LAB1: comments added

--- src/IE3_ADL_LAB1/com/mhelghamrawy/ArithmeticTerm.java
+++ src/IE3_ADL_LAB1/com/mhelghamrawy/ArithmeticTerm.java
@@ -1,10 +1,14 @@
 package IE3_ADL_LAB1.com.mhelghamrawy;
 
+import java.util.EmptyStackException;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class ArithmeticTerm {
 
+    // a String to hold the value of the expression to be computed
     private final String expression;
+    // a Stack<String> to hold the reversed expression computed by reverse()
     private final Stack<String> reversedExpression;
 
     {
@@ -20,6 +24,14 @@ public class ArithmeticTerm {
         return expression;
     }
 
+    /*
+     * A method which tokenizes the String with the arithmetic term,
+     * and uses a stack of Strings (java.util.Stack<String>) to store the tokens of the arithmetic expression,
+     * and writes it in the opposite order to the string instance variable.
+     * Called during constructor call.
+     *
+     * @author Mohamed ElGhamrawy
+     * */
     public void reverse() {
         String[] tokenizer;
         tokenizer = expression.split(" ");
@@ -29,6 +41,21 @@ public class ArithmeticTerm {
         }
     }
 
+    /*
+     * A method which uses a stack of doubles to evaluate the postfix expression with
+     * an extended version of the algorithm postfix2value that supports the following binary operators:
+     * addition (+),
+     * subtraction (-),
+     * multiplication (*),
+     * division (/),
+     * and exponentiation ();
+     * and the following unary operators:
+     * square-rooting (sqrt), and factorialization (!).
+     *
+     * @return bottom a double which exists at the bottom of the stack at the end of execution, i.e. the answer
+     *
+     * @author Mohamed ElGhamrawy
+     * */
     public double evaluate() {
         StackOfDoubles operandStack;
         operandStack = new StackOfDoubles();
@@ -54,10 +81,10 @@ public class ArithmeticTerm {
                 operandStack.push(Math.sqrt(operandStack.pop()));
             // Exponentiation
             else if (element.equals("^")) {
-                double base;
-                base = operandStack.pop();
+                double power;
+                power = operandStack.pop();
 
-                operandStack.push(Math.pow(base, operandStack.pop()));
+                operandStack.push(Math.pow(operandStack.pop(), power));
             }
             // Factorials
             else if (element.endsWith("!")) {
@@ -85,9 +112,106 @@ public class ArithmeticTerm {
             throw new IllegalStateException("Incorrect number of arguments given.");
     }
 
+    /*
+     * A method which assists the evaluate() method by calculating factorials recursively.
+     *
+     * @parameters n an integer to be factorialized
+     * @return  an integer with the value of n!
+     *
+     * @author Mohamed ElGhamrawy
+     * */
     private int factorial(int n) {
         return (n == 1 || n == 0) ? 1 : n*factorial(n-1);
 
+    }
+
+    /*
+    * A method which uses a stack of strings to convert the FPAE into a postfix expression with
+    * a modified version of the algorithm infix2postfix that supports unary operators as well
+    *
+    * @return postFixExpression a string containing the converted expression
+    *
+    * @author Hussam Kayed
+    * */
+    public String convert()
+    {
+        StringTokenizer token = new StringTokenizer(this.expression);
+        Stack<String> operatorStack = new Stack<>();
+        String postFixExpression = "";
+
+        while(token.hasMoreTokens())
+        {
+            String currentToken = token.nextToken();
+            if(checkDouble(currentToken))
+            {
+                postFixExpression += currentToken + " ";
+            }
+            else if(operators(currentToken))
+            {
+                operatorStack.push(currentToken);
+            }
+            else if(currentToken.equals(")"))
+            {
+                try
+                {
+                    postFixExpression += operatorStack.pop() + " ";
+                } catch (EmptyStackException exception)
+                {
+                    System.out.println("Please check the syntax of the inserted arithmetic Expression");
+                }
+            }
+            else
+                continue;
+
+        }
+        return postFixExpression;
+    }
+
+    /*
+     * A method which assists the convert() method by checking if a token is an operator.
+     *
+     * @parameters token a String holding the token to be checked
+     * @return  a boolean, with true meaning that the token is an operator
+     *
+     * @author Hussam Kayed
+     * */
+    public boolean operators(String token)
+    {
+        if(token.equals("*"))
+            return true;
+        else if(token.equals("+"))
+            return true;
+        else if(token.equals("e"))
+            return true;
+        else if(token.equals("-"))
+            return true;
+        else if(token.equals("/"))
+            return true;
+        else if(token.equals("sqrt"))
+            return true;
+        else if(token.equals("^"))
+            return true;
+        else
+            return false;
+    }
+    /*
+     * A method which assists the convert() method by checking if a token is an operand of type double.
+     *
+     * @parameters token a String holding the token to be checked
+     * @return  a boolean, with true meaning that the token is an operand of type double.
+     *
+     * @author Hussam Kayed
+     * */
+    public boolean checkDouble(String token)
+    {
+        try
+        {
+            Double.parseDouble(token);
+        } catch(NumberFormatException exception)
+        {
+            return false;
+        }
+        return true;
     }
 
     public Stack<String> getReversedExpression() {
--- src/IE3_ADL_LAB1/com/mhelghamrawy/Main.java
+++ src/IE3_ADL_LAB1/com/mhelghamrawy/Main.java
@@ -3,6 +3,14 @@ package IE3_ADL_LAB1.com.mhelghamrawy;
 public class Main {
 
     public static void main(String[] args) {
+        /*
+        SUBMISSION
+        * */
+        testConvertor();
+
+        /*
+        PREPARATION
+        * */
         testConstructor();
         testTokenizer();
 
@@ -12,8 +20,13 @@ public class Main {
     private static void testConstructor() {
         ArithmeticTerm term;
         term = new ArithmeticTerm("5.1 9 8.88 + 4 sqrt 6 / ^ 7 - *");
+        ArithmeticTerm term1 = new ArithmeticTerm("4 sqrt 6 /");
+        ArithmeticTerm term2 = new ArithmeticTerm("9 8.88 + 4 sqrt 6 / ^");
+
 
         System.out.println(term.toString());
+        System.out.println(term1.evaluate());
+        System.out.println(term2.evaluate());
         System.out.println(term.evaluate());
     }
 
@@ -27,5 +40,14 @@ public class Main {
 
         System.out.println(term1.getReversedExpression());
         System.out.println(term2.getReversedExpression());
+    }
+
+    // task 2.3
+    private static void testConvertor() {
+        ArithmeticTerm term1 = new ArithmeticTerm("( 5.1 * ( ( ( 9 + 8.88 ) ^ ( ( sqrt 4 ) / 6 ) ) - 7  ) )");
+        ArithmeticTerm term2 = new ArithmeticTerm(term1.convert());
+        term2.evaluate();
+
+        System.out.println(term2);
     }
 }
